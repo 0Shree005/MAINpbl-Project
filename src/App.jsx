9d5965cbebcd721250f1eab610fd89695a1485c5
0Shree005 <shreepingale12@gmail.com>
2024-02-20 18:09:08 +0530

    added dynamic content switching b/w pages, and added some gradients to the content in the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-// import { useState } from 'react'
-import './App.css'
 import Navbar from "./Navbar"
 import "./styles.css"
+import './App.css'
+import Home from "./pages/home";
+import Library from "./pages/library";
+
 
 function App() {
-  // const [count, setCount] = useState(0)
+  let component
+  switch(window.location.pathname){
+    
+    case "/vite-deploy-demo/home":
+      component = <Home/>
+      
+      break
 
+    case "/vite-deploy-demo/library":
+      component = <Library />
+      break    
+  }
   return (
+    <>
       <Navbar />
+      <div className="container">{component}</div>
+      
+    </>
   )
 }
 
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,11 @@
 export default function Navbar () {
     return <nav className="nav">
-        <a href="/" className="Site_Name">Literary Moisac</a>
+        <a href="/vite-deploy-demo/home" className="Site_Name">Literary Moisac</a>
         <ul>
-            {/* <li>
+            <li className="active" >
                 <a href="/vite-deploy-demo/home">home</a>
-            </li> */}
-            <li>
+            </li>
+            <li >
                 <a href="/vite-deploy-demo/library">library</a>
             </li>
         </ul>
